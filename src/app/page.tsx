6b60722b6349a6646a1c1381604141adcfd9c7fc
Archishman Mitra <riju.archishman@gmail.com>
2024-10-24 04:38:15 +0530

    auth flow and account creation to be pushed into db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import LinkAccountButton from "@/components/LinkAccountButton";
+
 
 export default async function Home() {
 
   return (
-    <h1 className="text-red-600">hello world</h1>
+    <LinkAccountButton/>
+   
   );
 }
